5781e2d5ac58d45d35c72fbb2489c4dc45e63577
herold1989 <jens_herold@gmx.net>
2022-07-14 23:58:41 +0200

    Navbar section ready

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { BiSearch } from 'react-icons/bi'
 import { BsPerson } from 'react-icons/bs'
 import { HiOutlineMenuAlt4 } from 'react-icons/hi'
@@ -7,8 +7,11 @@ import {FaFacebook, FaInstagram, FaTwitter,FaPinterest, FaYoutube} from 'react-i
 import './NavbarStyles.css'
 
 function Navbar() {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => setNav(!nav)
+    
     return (
-        <div className='navbar'>
+        <div className={nav ? 'navbar navbar-bg' : 'navbar'}>
             <div className="logo">
                 <h2>
                     BEACHES.
@@ -22,14 +25,14 @@ function Navbar() {
                 <li>Views</li>
             </ul>
             <div className='nav-icons'>
-                <BiSearch className='icon' />
+                <BiSearch className='icon' style={{marginRight: '1rem'}} />
                 <BsPerson className='icon' />
             </div>
-            <div className='hamburger'>
+            <div className='hamburger' onClick={handleNav}>
                 <HiOutlineMenuAlt4 className='icon' />
             </div>
 
-            <div className='mobile-menu'>
+            <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
                 <ul className="mobile-nav">
                     <li>Home</li>
                     <li>Destinations</li>
