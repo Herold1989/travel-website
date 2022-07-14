fefa7e2fb6945d27bd4d809ec81d0ce797a9b03f
herold1989 <jens_herold@gmx.net>
2022-07-14 23:36:38 +0200

    adding navbar stylings

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,29 +1,60 @@
 import React from 'react'
-import {BiSearch} from 'react-icons/bi'
-import {BsPerson} from 'react-icons/bs'
+import { BiSearch } from 'react-icons/bi'
+import { BsPerson } from 'react-icons/bs'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import {FaFacebook, FaInstagram, FaTwitter,FaPinterest, FaYoutube} from 'react-icons/fa'
 
+import './NavbarStyles.css'
 
 function Navbar() {
-  return (
-    <div className='navbar'>
-        <div className = "logo">
-            <h2>
-                BEACHES
-            </h2>
+    return (
+        <div className='navbar'>
+            <div className="logo">
+                <h2>
+                    BEACHES.
+                </h2>
+            </div>
+            <ul className='nav-menu'>
+                <li>Home</li>
+                <li>Destinations</li>
+                <li>Travel</li>
+                <li>Book</li>
+                <li>Views</li>
+            </ul>
+            <div className='nav-icons'>
+                <BiSearch className='icon' />
+                <BsPerson className='icon' />
+            </div>
+            <div className='hamburger'>
+                <HiOutlineMenuAlt4 className='icon' />
+            </div>
+
+            <div className='mobile-menu'>
+                <ul className="mobile-nav">
+                    <li>Home</li>
+                    <li>Destinations</li>
+                    <li>Travel</li>
+                    <li>Book</li>
+                    <li>Views</li>
+                </ul>
+                <div className='mobile-menu-bottom'>
+                    <div className='menu-icons'>
+                        <button>Search</button>
+                        <button>Account</button>
+
+                    </div>
+                    <div className='social-icons'>
+                        <FaFacebook className='icon' />
+                        <FaInstagram className='icon' />
+                        <FaTwitter className='icon' />
+                        <FaPinterest className='icon' />
+                        <FaYoutube className='icon' />
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <ul className='nav-menu'>
-            <li>Home</li>
-            <li>Destinations</li>
-            <li>Travel</li>
-            <li>Book</li>
-            <li>Views</li>
-        </ul>
-        <div className='nav-icons'>
-            <BiSearch className='icon'/>
-            <BsPerson className='icon'/>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
