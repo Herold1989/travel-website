2be04b45867c40f064e211a1fdaf4ad467d49d5f
herold1989 <jens_herold@gmx.net>
2022-07-15 13:55:03 +0200

    finishing destinations section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Destinations from "./components/destinations/Destinations";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
     <Navbar/>
     <Hero/>
+    <Destinations/>
     </div>
     );
 }
