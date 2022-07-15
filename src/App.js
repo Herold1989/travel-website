e8456757aacc011a8caf94ee3241d1bce60ffde1
herold1989 <jens_herold@gmx.net>
2022-07-15 12:42:45 +0200

    add hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+
 
 function App() {
   return (
     <div>
     <Navbar/>
+    <Hero/>
     </div>
     );
 }
