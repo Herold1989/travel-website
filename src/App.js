b187b8b40adfda923a98c4ab8a73598662b0f4a4
herold1989 <jens_herold@gmx.net>
2022-07-15 16:27:22 +0200

    adding 'carousel' to page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/hero/Hero";
 import Destinations from "./components/destinations/Destinations";
 import Search from "./components/search/Search";
 import Selects from "./components/selects/Selects";
+import ImgCarousel from "./components/carousel/ImgCarousel";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
     <Destinations/>
     <Search/>
     <Selects/>
+    <ImgCarousel/>
     </div>
     );
 }
---
+++ src/components/carousel/ImgCarousel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ImgCarouselStyles.css'
+
+function ImgCarousel() {
+  return (
+    <div>ImgCarousel</div>
+  )
+}
+
+export default ImgCarousel
