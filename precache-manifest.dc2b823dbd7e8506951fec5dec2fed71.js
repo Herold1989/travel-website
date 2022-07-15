a997969151c64021989e22243560104882150f90
herold1989 <jens_herold@gmx.net>
2022-07-15 17:15:51 +0200

    Updates

---
+++ precache-manifest.dc2b823dbd7e8506951fec5dec2fed71.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "25e24f3d1cee6b89dac2",
+    "url": "/travel-website/static/css/main.3d50a4e3.chunk.css"
+  },
+  {
+    "revision": "25e24f3d1cee6b89dac2",
+    "url": "/travel-website/static/js/main.25e24f3d.chunk.js"
+  },
+  {
+    "revision": "6b694279257308c001f2",
+    "url": "/travel-website/static/css/1.fb0ce254.chunk.css"
+  },
+  {
+    "revision": "6b694279257308c001f2",
+    "url": "/travel-website/static/js/1.6b694279.chunk.js"
+  },
+  {
+    "revision": "8ca6051993b0d36b5ed6",
+    "url": "/travel-website/static/js/runtime~main.8ca60519.js"
+  },
+  {
+    "revision": "06041dfeb9cd84e866ffdd17c467a72f",
+    "url": "/travel-website/static/media/select_img1.06041dfe.jpg"
+  },
+  {
+    "revision": "bfbf4c75816237fcc0d65312bb7c60b7",
+    "url": "/travel-website/static/media/select_img2.bfbf4c75.jpg"
+  },
+  {
+    "revision": "e14f66534af7105ae9312d8a2e26abbd",
+    "url": "/travel-website/static/media/select_img3.e14f6653.jpg"
+  },
+  {
+    "revision": "578e9083617acd28fdf2e9a2b4312bba",
+    "url": "/travel-website/static/media/select_img4.578e9083.jpg"
+  },
+  {
+    "revision": "5c40b11e8459b021c5e5ab1a35ecbd12",
+    "url": "/travel-website/static/media/select_img5.5c40b11e.jpg"
+  },
+  {
+    "revision": "3ee3030a31f466ff683950ad00cdb8d5",
+    "url": "/travel-website/static/media/maldives3.3ee3030a.jpg"
+  },
+  {
+    "revision": "772a1e63b559c5cb3281bf5271a12644",
+    "url": "/travel-website/static/media/maldivesVideo.772a1e63.mp4"
+  },
+  {
+    "revision": "aa0717a9ec3848708b794b9df071c68c",
+    "url": "/travel-website/static/media/borabora.aa0717a9.jpg"
+  },
+  {
+    "revision": "ebfb013d31385aac9d5b3e4e4e91b404",
+    "url": "/travel-website/static/media/borabora2.ebfb013d.jpg"
+  },
+  {
+    "revision": "6fb79e9aaa2645938a55edd53f2b7320",
+    "url": "/travel-website/static/media/keywest.6fb79e9a.jpg"
+  },
+  {
+    "revision": "8af96b9c53452d3c184902983bc4a446",
+    "url": "/travel-website/static/media/maldives.8af96b9c.jpg"
+  },
+  {
+    "revision": "4600e513a3254f006f698f6cf5a499f4",
+    "url": "/travel-website/static/media/maldives2.4600e513.jpg"
+  },
+  {
+    "revision": "2d5f23d58e2ac4c549dcbadcad6bbdef",
+    "url": "/travel-website/static/media/gold.2d5f23d5.png"
+  },
+  {
+    "revision": "b3fd458788391c9f23d13b90194ea353",
+    "url": "/travel-website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/travel-website/precache-manifest.dc2b823dbd7e8506951fec5dec2fed71.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/travel-website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
